10362a2c387eca34c7d9b0653b7ad6a749af37b6
airewatterson <airewatterson@gmail.com>
2022-12-11 07:06:20 +0000

    Add scripts & testing.

---
+++ out/xo-helloworld.js
@@ -0,0 +1,4 @@
+"use strict";
+// A hello world test for xo
+// Print a message to the console: "Hello World!"
+console.log('Hello World!');
---
+++ src/xo-helloworld.ts
@@ -0,0 +1,5 @@
+// A hello world test for xo
+
+// Print a message to the console: "Hello World!"
+
+console.log('Hello World!');
