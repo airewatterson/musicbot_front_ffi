a636b16b978f2079c472a39eeaec33e19fe0324c
airewatterson <airewatterson@gmail.com>
2022-12-12 08:41:10 +0000

    Re-import all.

--- out/xo-helloworld.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// A hello world test for xo
-// Print a message to the console: "Hello World!"
-console.log('Hello World!');
--- src/xo-helloworld.ts
+++
@@ -1,5 +0,0 @@
-// A hello world test for xo
-
-// Print a message to the console: "Hello World!"
-
-console.log('Hello World!');
